bc0b09c5907abb63f2795b3a63a02e38f282f871
maze88 <michael.52@msn.com>
2018-04-24 14:24:58 +0300

    Minor edits to readme.

--- README.md
+++ README.md
@@ -6,9 +6,8 @@
 * **Homepage**: https://www.maze88.xyz
 
 ---
-
-This cheatsheet consists of Node.js expressions that demonstrate basic usage.
-It is intended for (and made by) entry level users, for example learning Node.js as their second programming language - like myself!
-All code here is executable except for code requiring external modules (see `/* Packages */` section, in code).
-
+This cheatsheet consists of functional Node.js expressions that demonstrate basic usage.
+Some expressions are embedded with examples of other types of expressions in them, giving the cheatsheet characteristics of a demo more than of a documentation.
+It is intended for entry level users, for example learning Node.js as their first or second programming language.
+All code here is executable except for code requiring external modules (see '/* Packages */', in code).
 ---
--- node.js_cheatsheet.js
+++ node.js_cheatsheet.js
@@ -4,8 +4,9 @@ Twitter: @maze_52
 Github: @maze88
 Homepage: https://www.maze88.xyz
 
-This cheatsheet consists of functional Node.js expressions that demonstrate basic usage. Some expressions contain other types of expressions in them, giving the cheatsheet more of a program look and feel (and less of a documentation look and feel).
-It is intended for entry level users, for example learning Node.js as their second programming language.
+This cheatsheet consists of functional Node.js expressions that demonstrate basic usage.
+Some expressions are embedded with examples of other types of expressions in them, giving the cheatsheet characteristics of a demo more than of a documentation.
+It is intended for entry level users, for example learning Node.js as their first or second programming language.
 All code here is executable except for code requiring external modules (see '/* Packages *\/', below).
 */
 
